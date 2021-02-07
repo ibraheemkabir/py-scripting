9de2e1218e7ce91dd9cb1718c236d4a58f6442e6
ibraheemkabir <ibraheemkabir9@gmail.com>
2021-02-07 20:14:40 +0100

    slicing

--- basics.py
+++ basics.py
@@ -31,5 +31,24 @@ mean = mysum/length
 monday_temperatures = (1,4,5)
 
 tup = 'tuple is an ordered collection of objects'
+
+tuesday_temperatures = [9.1, 8.8, 7.5]
+
+seconds = [1.2323442655, 1.4534345567, 1.023458894, 1.10001399445]
+
+seconds.remove(1.4534345567, 1.023458894, 1.10001399445)
+
+tuesday_temperatures.index(8.8)
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+
+tuesday_temperatures[1:2]
+tuesday_temperatures[1:]
+tuesday_temperatures[-1]
+
+mystring = 'hello'
+
+mystring[0]
+
+print(serials[0], serials[2], serials[5])
 print(monday_temperatures)
 print(mean)
